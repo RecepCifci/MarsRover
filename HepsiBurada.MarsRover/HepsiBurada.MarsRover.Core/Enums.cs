51f1ffdf0b2063a27904bcfbf048fbedec848ec6
RecepCifci <recepcifci65@gmail.com>
2022-01-08 16:29:48 +0300

    Create Project

---
+++ HepsiBurada.MarsRover/HepsiBurada.MarsRover.Core/Enums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiBurada.MarsRover.Core
+{
+    public class Enums
+    {
+        public enum Direction
+        {
+            North,
+            East,
+            South,
+            West
+        }
+    }
+}
